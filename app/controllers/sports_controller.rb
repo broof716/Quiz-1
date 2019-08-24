f0a4153b74661ff76cec1781fb0b0f1a38ed22b3
Bryan Roof <broof716@gmail.com>
2019-08-24 09:06:05 +0000

    working on background

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -2,4 +2,20 @@ class SportsController < ApplicationController
   def index
     @sports = Sport.all
   end
+
+  def new
+    @sport = Sport.new
+  end
+
+  def create
+    Sport.create(sport_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def sport_params
+    params.require(:sport).permit(:name, :description)
+  end
+
 end
