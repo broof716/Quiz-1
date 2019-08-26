a8b38f0cc3a0eaffe6512b70afb17232862132a7
Bryan Roof <broof716@gmail.com>
2019-08-26 05:30:59 +0000

    background image complete

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,20 @@
 class TeamsController < ApplicationController
+  def index
+    @sports = Sport.all
+  end
+
+  def new
+    @sport = Sport.new
+  end
+
+  def create
+    Sport.create(sport_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def sport_params
+    params.require(:sport).permit(:name, :description)
+  end
 end
